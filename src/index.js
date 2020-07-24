43fe3117efa2770d5163d3212f3db11aa44023f3
David Snyder <davidgraygs4@gmail.com>
2020-07-23 20:29:08 -0400

    moved discord events to seperate file

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ app.use("/", require("./routes/index"));
 
 // get the initialized clients from another file
 const { DiscordClient, TwitchClient } = require("./utils/initClients");
+const DiscordEvents = require("./DiscordEvents.js");
 
 // initialize the object that will store all sockets currently connected
 const sockets = {};
@@ -60,129 +61,11 @@ io.origins((origin, callback) => {
 TwitchEvents(TwitchClient, sockets, app);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// DISCORD MESSAGE HANDLING
+// DISCORD
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// TODO: move discord events to separate file
-DiscordClient.on("message", async message => {
-	if (message.guild == null) return;
-	// if the message was sent by a bot it should be ignored
-	if (message.author.bot) return;
-	if (!sockets.hasOwnProperty(message.guild.id)) return;
-
-	const { liveChatId } = [...sockets[message.guild.id]][0].userInfo;
-
-	// don't waste time with the rest of the stuff if there isn't a connection to this guild
-	if (message.channel.id != liveChatId && !liveChatId.includes(message.channel.id)) return;
-
-	const senderName = message.member.displayName;
-
-	const badges = {};
-
-	if (message.guild.ownerID == message.author.id) {
-		badges["owner"] = {
-			image: "https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1",
-			title: "Server Owner",
-		};
-	} else {
-		if (message.member.hasPermission(["MANAGE_MESSAGES"])) {
-			badges["moderator"] = {
-				image: "https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1",
-				title: "Moderator",
-			};
-		}
-	}
-
-	if (message.member.premiumSinceTimestamp) {
-		badges["booster"] = {
-			image: "https://cdn.discordapp.com/attachments/711241287134609480/727707559045365771/serverbooster.png",
-			title: "Server Booster",
-		};
-	}
-
-	if (ranks.discord.developers.includes(message.author.id)) {
-		badges["developer"] = {
-			image: "https://cdn.discordapp.com/attachments/699812263670055052/722630142987468900/icon_18x18.png",
-			title: "DisStreamchat Staff",
-		};
-	}
-
-	try {
-		const CleanMessage = message.cleanContent;
-		// const plainMessage = formatMessage(CleanMessage, "discord", {});
-		const HTMLCleanMessage = await formatMessage(CleanMessage, "discord", {}, { HTMLClean: true });
-		// const censoredMessage = formatMessage(CleanMessage, "discord", {}, { censor: true });
-		// const HTMLCensoredMessage = formatMessage(CleanMessage, "discord", {}, { HTMLClean: true, censor: true });
-
-		const messageObject = {
-			displayName: senderName,
-			username: message.author.username,
-			userId: message.author.id,
-			avatar: message.author.displayAvatarURL(),
-			body: HTMLCleanMessage,
-			// HTMLCleanMessage,
-			// censoredMessage,
-			// HTMLCensoredMessage,
-			platform: "discord",
-			messageId: "",
-			messageType: "chat",
-			uuid: message.id,
-			id: message.id,
-			badges,
-			sentAt: message.createdAt.getTime(),
-			// TODO: improve with roles
-			userColor: message.member.displayHexColor === "#000000" ? "#FFFFFF" : message.member.displayHexColor,
-		};
-
-		if (messageObject.body.length <= 0) return;
-
-		const _ = [...sockets[message.guild.id]].forEach(async s => await s.emit("chatmessage", messageObject));
-	} catch (err) {
-		console.log(err.message);
-	}
-});
-
-DiscordClient.on("messageDelete", message => {
-	try {
-		if (sockets.hasOwnProperty(message.guild.id)) [...sockets[message.guild.id]].forEach(async s => await s.emit("deletemessage", message.id));
-	} catch (err) {
-		console.log(err.message);
-	}
-});
-
-DiscordClient.on("messageDeleteBulk", message => {
-	message.forEach(msg => {
-		try {
-			if (sockets.hasOwnProperty(msg.guild.id)) [...sockets[msg.guild.id]].forEach(async s => await s.emit("deletemessage", msg.id));
-		} catch (err) {
-			console.log(err.message);
-		}
-	});
-});
-
-DiscordClient.on("messageUpdate", async (oldMsg, newMsg) => {
-	if (!sockets.hasOwnProperty(newMsg.channel.guild.id)) return;
-	const { liveChatId } = [...sockets[newMsg.channel.guild.id]][0].userInfo;
-	if (newMsg.channel.id != liveChatId && !liveChatId.includes(newMsg.channel.id)) return;
-	try {
-		const HTMLCleanMessage = await formatMessage(newMsg.cleanContent, "discord", {}, { HTMLClean: true });
-		const updateMessage = {
-			body: HTMLCleanMessage,
-			id: newMsg.id,
-		};
-		const _ = [...sockets[newMsg.channel.guild.id]].forEach(async s => await s.emit("updateMessage", updateMessage));
-	} catch (err) {
-		console.log(err.message);
-	}
-});
-
-DiscordClient.on("guildMemberAdd", async member => {
-	if (member.guild.id === "711238743213998091") {
-		await member.send(
-			`Welcome to the DisStreamChat community. If you need help setting up DisStreamChat feel free to ask in any of the help channels. But try to find a help channel related to you problem. If you can't talk in any of the chats just DM a moderator and they will sort out the issue. Any suggestions or bug reports you have should go in the respective channels. 😀`
-		);
-	}
-});
+// see ./DiscordEvents.js
+DiscordEvents(DiscordClient, sockets, app)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Youtube Events
