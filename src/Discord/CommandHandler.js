3e64ee2f0ad8acafd0a4b23b184000666e1a1a41
David Snyder <davidgraygs4@gmail.com>
2020-07-25 13:17:12 -0400

    added custom commands and added function to add them

--- src/Discord/CommandHandler.js
+++ src/Discord/CommandHandler.js
@@ -1,28 +1,44 @@
-const path = require("path")
-const fs = require("fs")
-const {adminWare, modWare} = require("../utils/functions")
-const commandPath = path.join(__dirname, "Commands")
-const commandFiles = (fs.readdirSync(commandPath))
-const commands = {}
-commandFiles.forEach(command => {
-    if(command.endsWith(".js")){
-        const commandObj = require(path.join(commandPath, command))
-        const _ = [commandObj.name, ...commandObj.aliases].map(name => {
-            commands[name] = commandObj
-        }) 
-    }
-})
+const path = require("path");
+const fs = require("fs");
+const { adminWare, modWare } = require("../utils/functions");
+const commandPath = path.join(__dirname, "Commands");
+const commandFiles = fs.readdirSync(commandPath);
+const commands = {};
 
-const prefix = "!"
+commandFiles.forEach(command => {
+	if (command.endsWith(".js")) {
+		const commandObj = require(path.join(commandPath, command));
+		const _ = [commandObj.name, ...commandObj.aliases].map(name => {
+			commands[name] = commandObj;
+		});
+	}
+});
+
+// the admin app has already been initialized in routes/index.js
+const admin = require("firebase-admin");
+
+const prefix = "!";
 
 module.exports = async (message, client) => {
-    if(!message.content.startsWith(prefix)) return
-    const args = message.content.split(" ")
-    const command = args.shift().slice(1)
-    const commandObj = commands[command]
-    if(!commandObj) return
-    
-    if(commandObj.adminOnly) await adminWare(message, args, client, commandObj.execute)
-    else if(commandObj.permissions) await modWare(message, args, client, commandObj.permissions, commandObj.execute)
-    else await commandObj.execute(message, args, client)
-}
+	if (!message.content.startsWith(prefix)) return;
+	const args = message.content.split(" ");
+	const command = args.shift().slice(1);
+	const commandObj = commands[command];
+	if (!commandObj) {
+		const guildRef = await admin.firestore().collection("customCommands").doc(message.guild.id).get();
+		const guildData = guildRef.data();
+		if (guildData) {
+			for(const [key, value] of Object.entries(guildData)){
+                if(key === command || command === value.name || value.aliases.includes(command)){
+                    return await message.channel.send(value.message)
+                }
+            }
+		} else {
+			return;
+		}
+	} else {
+		if (commandObj.adminOnly) await adminWare(message, args, client, commandObj.execute);
+		else if (commandObj.permissions) await modWare(message, args, client, commandObj.permissions, commandObj.execute);
+		else await commandObj.execute(message, args, client);
+	}
+};
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -29,6 +29,29 @@ const modWare = async (msg, args, client, permissions, cb) => {
     }
 }
 
+const resolveUser = (msg, username) => {
+    const memberCache = msg.guild.members.cache;
+    if (/<@!?\d+>/g.test(username)) {
+        return memberCache.get(msg.mentions.users.first().id);
+    }
+    if (memberCache.has(username)) {
+        return memberCache.get(username);
+    }
+    if (/(.*)#(\d{4})/g.test(username)) {
+        return memberCache.find(member => member.user.tag === username);
+    }
+    if (memberCache.find(member => member.nickname === username)) {
+        return memberCache.find(member => member.nickname === username);
+    }
+    if (memberCache.find(member => member.user.username === username)) {
+        return memberCache.find(member => member.user.username === username);
+    }
+    if (memberCache.find(member => member.id === username)) {
+        return memberCache.find(member => member.id === username);
+    }
+    return null;
+};
+
 const adminWare = async (message, args, client, cb) => {
     const discordAdmins = adminIds.discord.developers
     if(discordAdmins.includes(message.author.id)){
@@ -90,5 +113,6 @@ module.exports = {
     walkSync,
     Command,
     modWare,
-    adminWare
+    adminWare,
+    resolveUser
 }
