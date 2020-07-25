2c6e168ffc471fefe0fafb80f98615cb63866aec
David Snyder <davidgraygs4@gmail.com>
2020-07-25 13:35:25 -0400

    allow deleting commands

--- src/Discord/Commands/commands.js
+++ src/Discord/Commands/commands.js
@@ -7,23 +7,24 @@ module.exports = {
 	description: "command to manage custom commands",
 	permissions: ["MANAGE_MESSAGES"],
 	execute: async (message, args, client) => {
+		const guildRef = await admin.firestore().collection("customCommands").doc(message.guild.id).get();
+		const guildData = guildRef.data();
 		switch (args[0]) {
 			case "add":
 				const name = args[1];
 				const customMessage = args.slice(2).join(" ");
-				const guildRef = await admin.firestore().collection("customCommands").doc(message.guild.id).get();
-				const guildData = guildRef.data();
+
 				if (Object.keys(guildData || {}).includes(name)) {
-					return await message.channel.send(`The command ${name} already exists`);
+					return await message.channel.send(`The command "${name}" already exists`);
 				}
-				if(!guildData){
+				if (!guildData) {
 					await admin
 						.firestore()
 						.collection("customCommands")
 						.doc(message.guild.id)
 						.set({
 							[name]: {
-                                name,
+								name,
 								aliases: [],
 								message: customMessage,
 							},
@@ -35,18 +36,31 @@ module.exports = {
 						.doc(message.guild.id)
 						.update({
 							[name]: {
-                                name,
+								name,
 								aliases: [],
 								message: customMessage,
 							},
 						});
-                }
-                await message.channel.send(`The command "${name}" has been added successfully.`)
+				}
+				await message.channel.send(`The command "${name}" has been added successfully.`);
 
 				break;
 
+			case "delete":
+				const nameToDelete = args[1];
+				console.log(guildData);
+				if (!Object.keys(guildData || {}).includes(nameToDelete)) {
+					return await message.channel.send(`The command "${nameToDelete}" doesn't exist`);
+				} else {
+					const copy = { ...guildData };
+					delete copy[nameToDelete];
+					await admin.firestore().collection("customCommands").doc(message.guild.id).set(copy);
+				}
+				await message.channel.send(`The command "${nameToDelete}" has been deleted successfully.`);
+				break;
+
 			default:
-				await message.channel.send("❌ Invalid parameter specified. Possible parameters: add/delete/edit");
+				await message.channel.send("❌ Invalid parameter specified. Possible parameters: add and delete");
 		}
 	},
 };
