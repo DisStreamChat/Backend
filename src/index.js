f0fdcb4fdb65cbc4abd61406720a9f0fa6f03e83
David Snyder <davidgraygs4@gmail.com>
2020-07-27 12:22:42 -0400

    send delete commands through the users account

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const bodyParser = require("body-parser");
 const helemt = require("helmet");
 const TwitchEvents = require("./Twitch/TwitchEvents.js");
 const crypto = require("crypto");
-const ranks = require('./ranks.json');
+const ranks = require("./ranks.json");
+const fetch = require("node-fetch");
+const tmi = require("tmi.js");
 const { getAllEvents, listenMessages } = require("./routes/youtubeMessages");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,20 +41,22 @@ app.use(
 
 // add the routes stored in the 'routes' folder to the app
 app.use("/", require("./routes/index"));
-app.use("/public", express.static("public"))
-app.use("/images", express.static("images"))
+app.use("/public", express.static("public"));
+app.use("/images", express.static("images"));
 
 // get the initialized clients from another file
 const { DiscordClient, TwitchClient } = require("./utils/initClients");
 const DiscordEvents = require("./Discord/DiscordEvents.js");
+const admin = require("firebase-admin");
+const { ArrayAny } = require("./utils/functions.js");
 
 // initialize the object that will store all sockets currently connected
 const sockets = {};
 
 io.origins((origin, callback) => {
-	console.log(origin)
-	callback(null, true)
-})
+	console.log(origin);
+	callback(null, true);
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TWITCH
@@ -66,7 +70,7 @@ TwitchEvents(TwitchClient, sockets, app);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // see ./DiscordEvents.js
-DiscordEvents(DiscordClient, sockets, app)
+DiscordEvents(DiscordClient, sockets, app);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Youtube Events
@@ -83,11 +87,11 @@ app.get("/youtube/events", async (req, res, next) => {
 
 let listening = false;
 async function listenChat(channelId) {
-    if(!channelId){
-        return {
-            listening: false
-        }
-    }
+	if (!channelId) {
+		return {
+			listening: false,
+		};
+	}
 	if (listening) {
 		return {
 			listening: true,
@@ -101,15 +105,15 @@ async function listenChat(channelId) {
 		} = liveEvent;
 		const listener = listenMessages(liveChatId);
 		listener.on("messages", async newMessages => {
-            if(!sockets.hasOwnProperty(channelId)) return
-            newMessages = newMessages.sort((a, b) => a.publishedAt - b.publishedAt);
-            newMessages.forEach(message => {
-                const _ = [...sockets[channelId]].forEach(async s => await s.emit("chatmessage", message));
-            })
+			if (!sockets.hasOwnProperty(channelId)) return;
+			newMessages = newMessages.sort((a, b) => a.publishedAt - b.publishedAt);
+			newMessages.forEach(message => {
+				const _ = [...sockets[channelId]].forEach(async s => await s.emit("chatmessage", message));
+			});
 		});
 		listener.on("event-end", data => {
 			listening = false;
-            if(!sockets.hasOwnProperty(channelId)) return
+			if (!sockets.hasOwnProperty(channelId)) return;
 			const _ = [...sockets[channelId]].forEach(async s => await s.emit("event-end", data));
 		});
 		return {
@@ -130,7 +134,6 @@ app.get("/youtube/listen", async (req, res) => {
 // SOCKET CONNECTION HANDLING
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 // add a socket to a set at an id (key)
 const addSocket = (socket, id) => {
 	if (id != undefined) {
@@ -148,13 +151,13 @@ io.on("connection", socket => {
 	socket.emit("imConnected");
 	// the addme event is sent from the frontend on load with the data from the database
 	socket.on("addme", message => {
-        const { TwitchName, guildId } = message;
-        const youtubeChannelId = message.youtubeChannelId
+		const { TwitchName, guildId } = message;
+		const youtubeChannelId = message.youtubeChannelId;
 		socket.userInfo = message;
 
-        if(youtubeChannelId){
-            addSocket(socket, youtubeChannelId);
-        }
+		if (youtubeChannelId) {
+			addSocket(socket, youtubeChannelId);
+		}
 		addSocket(socket, guildId);
 		addSocket(socket, TwitchName);
 		TwitchClient.join(TwitchName);
@@ -166,7 +169,8 @@ io.on("connection", socket => {
 		socket.userInfo = data;
 	});
 
-	socket.on("deletemsg - discord", async id => {
+	socket.on("deletemsg - discord", async data => {
+		let id = data.id || data;
 		const { guildId, liveChatId } = socket.userInfo;
 
 		const connectGuild = DiscordClient.guilds.resolve(guildId);
@@ -199,8 +203,8 @@ io.on("connection", socket => {
 			const member = connectGuild.members.resolve(user);
 			member.roles.add(muteRole);
 			const _ = [...sockets[guildId]].forEach(async s => await s.emit("purgeuser", member.nickname));
-            await new Promise(resolve => setTimeout(resolve, 300000))
-            member.roles.remove(muteRole);
+			await new Promise(resolve => setTimeout(resolve, 300000));
+			member.roles.remove(muteRole);
 		} catch (err) {
 			console.log(err.message);
 		}
@@ -211,18 +215,68 @@ io.on("connection", socket => {
 		const connectGuild = DiscordClient.guilds.resolve(guildId);
 		try {
 			console.log(`Banning ${user} - Discord`);
-			connectGuild.members.ban(user, {days: 1});
+			connectGuild.members.ban(user, { days: 1 });
 		} catch (err) {
 			console.log(err.message);
 		}
 	});
 
-	socket.on("deletemsg - twitch", async id => {
+	socket.on("deletemsg - twitch", async data => {
 		const { TwitchName } = socket.userInfo;
-		try {
-			TwitchClient.deletemessage(TwitchName, id);
-		} catch (err) {
-			console.log(err.message);
+		function botDelete(id) {
+			try {
+				TwitchClient.deletemessage(TwitchName, id);
+			} catch (err) {
+				console.log(err.message);
+			}
+		}
+		let id = data.id;
+
+		if (!id) {
+			botDelete(data);
+		} else {
+			const modName = data.modName;
+			const modRef = (await admin.firestore().collection("Streamers").where("TwitchName", "==", modName).get()).docs[0];
+			const modData = modRef.data();
+			if (!modData) {
+				botDelete(id);
+			} else {
+				try {
+					const modPrivateDataRef = await admin.firestore().collection("Streamers").doc(modData.uid).collection("twitch").doc("data").get();
+					const modPrivateData = modPrivateDataRef.data();
+					if (!modPrivateData) {
+						throw new Error("no twitch auth");
+					}
+					const refreshToken = modPrivateData.refresh_token;
+					const response = await fetch(`https://api.disstreamchat.com/twitch/token/refresh?token=${refreshToken}`);
+					const data = await response.json();
+					if (!data) {
+						throw new Error("bad refresh token");
+					}
+					const scopes = data.scope;
+					if (!ArrayAny(scopes, ["chat:edit", "chat:read", "channel:moderate"])) {
+						throw new Error("bad scopes");
+					}
+					const UserClient = new tmi.Client({
+						options: { debug: false },
+						connection: {
+							secure: true,
+							reconnect: true,
+						},
+						identity: {
+							username: modName,
+							password: data.access_token,
+						},
+						channels: [TwitchName],
+					});
+					await UserClient.connect();
+					await UserClient.deletemessage(TwitchName, id);
+					await UserClient.disconnect();
+				} catch (err) {
+					console.log(err.message);
+					botDelete(id);
+				}
+			}
 		}
 	});
 
