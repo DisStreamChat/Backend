320c6d9f1cc1c1feb7767823f1c3d23872f134d1
David Snyder <davidgraygs4@gmail.com>
2020-07-26 21:30:06 -0400

    remove unused file

--- src/Discord/Constants.js
+++
@@ -1,7 +0,0 @@
-exports.emojis = {
-    bloblseeping: '<:blobsleeping:660160968440938497>',
-    blobgamer: '<:blobgamer:660161120719470642>',
-    bloboutage: '<:bloboutage:660161241062309931>',
-    photoblob: '<:photoblob:660161410042691615>',
-    blobtea: '<:blobtea:660162050558787596>',
-};
