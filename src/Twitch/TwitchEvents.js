50d06d68f8af7b58952ba41f5a7b7c0b392f4b2b
David Snyder <davidgraygs4@gmail.com>
2020-08-08 14:30:46 -0400

    made channel points messageId back

--- src/Twitch/TwitchEvents.js
+++ src/Twitch/TwitchEvents.js
@@ -641,6 +641,7 @@ module.exports = (TwitchClient, sockets, app) => {
 								avatar: DisTwitchChatProfile,
 								body: message,
 								platform: "twitch",
+                                messageId: "subscription"
 								messageType: "channel-points",
 								uuid: id,
 								id,
