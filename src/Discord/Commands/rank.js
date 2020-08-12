67e2df40ef98c982ca3ef9f0e8d5c7762f7a9c8a
David Snyder <davidgraygs4@gmail.com>
2020-08-12 16:02:44 -0400

    minor change to leveling system

--- src/Discord/Commands/rank.js
+++ src/Discord/Commands/rank.js
@@ -1,12 +1,11 @@
 const { resolveUser, generateRankCard } = require("../../utils/functions");
 const { MessageAttachment } = require("discord.js");
 const path = require("path");
-const fs = require("fs")
+const fs = require("fs");
 // the admin app has already been initialized in routes/index.js
 const admin = require("firebase-admin");
 const { registerFont } = require("canvas");
 registerFont(path.join(__dirname, "../../../public/Poppins/Poppins-Regular.ttf"), { family: "Poppins" });
-
 
 module.exports = {
 	name: "rank",
@@ -14,21 +13,23 @@ module.exports = {
 	description: "get your level",
 	execute: async (message, args, client) => {
 		let user = resolveUser(message, args.join(" "));
-        if (!user) {
-            if(args.length){
-                message.channel.send("user not found, here are your stats")
-            }
-            user = message.member};
-        if (user.user.bot){
-            return await message.channel.send(`❌ ${user} is a bot and bot's don't level`)
-        }
+		let msg = "";
+		if (!user) {
+			if (args.length) {
+				msg = "user not found, here are your stats";
+			}
+			user = message.member;
+		}
+		if (user.user.bot) {
+			return await message.channel.send(`❌ ${user} is a bot and bots don't level`);
+		}
 		const guildRef = await admin.firestore().collection("Leveling").doc(message.guild.id).get();
 		const guildData = guildRef.data();
 		let userData = guildData[user.id];
 		if (!userData) userData = { xp: 0, level: 0 };
-        const rankCard = await generateRankCard(userData, user)
-        const attachment = new MessageAttachment(rankCard.toBuffer(), "card.png");
-        fs.writeFileSync(path.join(__dirname, `../../../images/${user.user.username}.png`), rankCard.toBuffer())
-		message.channel.send(attachment);
+		const rankCard = await generateRankCard(userData, user);
+		const attachment = new MessageAttachment(rankCard.toBuffer(), "card.png");
+		fs.writeFileSync(path.join(__dirname, `../../../images/${user.user.username}.png`), rankCard.toBuffer());
+		message.channel.send(msg, attachment);
 	},
 };
