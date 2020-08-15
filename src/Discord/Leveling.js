be0f97b73c22da80e870ae57505fbfa54bf6d71d
David Snyder <davidgraygs4@gmail.com>
2020-08-15 15:45:58 -0400

    make leveling work with new web dashboard

--- src/Discord/Leveling.js
+++ src/Discord/Leveling.js
@@ -2,15 +2,15 @@
 const admin = require("firebase-admin");
 const { Random } = require("../utils/functions");
 
-const { getXp } = require("../utils/functions")
+const { getXp } = require("../utils/functions");
 
 module.exports = {
-    handleLeveling: async message => {
+	handleLeveling: async message => {
 		const levelingDataRef = await admin.firestore().collection("Leveling").doc(message.guild.id).get();
-        const levelingData = levelingDataRef.data();
+		const levelingData = levelingDataRef.data();
 		if (levelingData) {
-            const levelingChannelId = levelingData["notifications"] || message.channel.id;
-            console.log(levelingChannelId)
+			const levelingChannelId = levelingData.type === 3 ? levelingData.notifications || message.channel.id : message.channel.id;
+			// console.log(levelingChannelId);
 			let userLevelingData = levelingData[message.author.id];
 			if (!userLevelingData) {
 				userLevelingData = { xp: 0, level: 0, cooldown: 0 };
@@ -22,22 +22,26 @@ module.exports = {
 				userLevelingData.cooldown = now;
 				userLevelingData.xp += Random(10, 20);
 				userLevelingData.xp = Math.floor(userLevelingData.xp);
-				let xpToNextLevel = getXp(userLevelingData.level+1)
-				if(userLevelingData.xp >= xpToNextLevel){
-                    userLevelingData.level++
-                    const levelupMessage = `Congrats ${message.author}, you leveled up to level ${userLevelingData.level+1}`
-                    try{
-                        const levelingChannel = await message.guild.channels.resolve(levelingChannelId)
-                        levelingChannel.send(levelupMessage)
-                    }catch(err){
-                        message.channel.send(levelupMessage)
-                    }
-                }
+				let xpToNextLevel = getXp(userLevelingData.level + 1);
+				if (userLevelingData.xp >= xpToNextLevel) {
+					userLevelingData.level++;
+					if (levelingData.type !== 1) {
+						const levelupMessage = (levelingData.message || "Congrats {player}, you leveled up to level {level}")
+							.replace("{player}", message.author)
+							.replace("{level}", userLevelingData.level + 1);
+						try {
+							const levelingChannel = await message.guild.channels.resolve(levelingChannelId);
+							levelingChannel.send(levelupMessage);
+						} catch (err) {
+							message.channel.send(levelupMessage);
+						}
+					}
+				}
 				levelingData[message.author.id] = userLevelingData;
 				await admin.firestore().collection("Leveling").doc(message.guild.id).update(levelingData);
 			}
 		} else {
-			await admin.firestore().collection("Leveling").doc(message.guild.id).set({});
+			// await admin.firestore().collection("Leveling").doc(message.guild.id).set({});
 		}
-	}
-}
+	},
+};
