c2d93999c0699a38ec452eba1c6d08a827004678
David Snyder <davidgraygs4@gmail.com>
2020-08-16 17:00:01 -0400

    remove temp change

--- src/routes/index.js
+++ src/routes/index.js
@@ -278,7 +278,7 @@ router.get("/emotes", async (req, res, next) => {
 
 router.get("/checkmod", async (req, res, next) => {
 	const channelName = req.query.channel;
-    return res.json(await Api.getUserInfo(channelName))
+    // return res.json(await Api.getUserInfo(channelName))
 	const userName = req.query.user;
 	try {
 		await TwitchClient.join("#" + channelName);
