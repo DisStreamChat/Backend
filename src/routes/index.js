9bbec1dba2ab03d3ceaeef4657e003576216e694
David Snyder <davidgraygs4@gmail.com>
2020-08-19 21:16:56 -0400

    for gods sake please work

--- src/routes/index.js
+++ src/routes/index.js
@@ -294,9 +294,7 @@ router.get("/checkmod", async (req, res, next) => {
 	} catch (err) {
         try{
             console.log("failed to join: ", err)
-            const results = await TwitchClient.mods(channelName);
-            console.log(results);
-            const isMod = !!userName && results.includes(userName.toLowerCase());
+            const isMod = TwitchClient.isMod(channelName, userName);
             TwitchClient.part(channelName)
             if (isMod) {
                 return res.json(await Api.getUserInfo(channelName));
