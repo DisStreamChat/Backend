d24ee456ea06036b582e6841b64f3d4405b3139e
KobeLiesenborgs <kobe.liesenborgs1998@gmail.com>
2020-08-22 21:53:21 +0200

    Adding in help and rps commands, modifying CommandHandler to comply

--- src/Discord/Commands/purge.js
+++ src/Discord/Commands/purge.js
@@ -7,20 +7,19 @@ module.exports = {
 	execute: async (message, args, client) => {
 		if(args.length === 0) return await message.channel.send("Please provide a number between 1 and 99.")
 		const amount = +args[0] + 1;
+		//TODO: add confirmation and functionality for 100+ deletions
 		if(amount > 100){
-			//TODO: queue up bigger bulk deletion
 			return message.channel.send("Maximum amount of messages to delete is 99.")
 		}
 		if(amount < 2){
 			return message.channel.send("Minimum amount of messages to delete is 1.")
 		}
 
-			//const messages = await msg.channel.messages.fetch({ limit: Number(args[0]) + 1 });
-			const messages = await message.channel.messages.fetch({ limit: amount });
-			for(message of messages.values()){
-				await message.delete();
-				await new Promise(resolve => setTimeout(resolve,300)) 
-			}
+		const messages = await message.channel.messages.fetch({ limit: amount });
+		for(message of messages.values()){
+			await message.delete();
+			await new Promise(resolve => setTimeout(resolve,300)) 
+		}
 		
 		const msg = await message.channel.send(`Deleted ${amount-1} messages`);
 		setTimeout(() => {
