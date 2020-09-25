b7f6b7e7042d9e08332f7cc09b3f798f635cd8a3
NotMrCube <matteo.ma.ad@gmail.com>
2020-09-25 10:01:22 +0200

    added a more detailed embed for the ping

--- src/Discord/Commands/ping.js
+++ src/Discord/Commands/ping.js
@@ -3,8 +3,19 @@ module.exports = {
 	aliases: ["🏓"],
 	description: "Ping the bot.",
 	execute: async (message, args, client) => {
-		const ping = await message.channel.send("Pong!");
 		message.react("🏓");
-		ping.edit(`Pong! Latency is \`${ping.createdTimestamp - message.createdTimestamp}ms\`. Bot ping is \`${client.ws.ping}ms\` 💓`);
+		let Pinging = new MessageEmbed()
+			.setTitle(`🏓 Pinging...`)
+
+		const msg = await message.channel.send(Pinging);
+		let pingembed = new MessageEmbed()
+			.setTitle(`🏓 Pong!`)
+			.addFields(
+				{ name: `**Ping:**`, value: `${Math.floor(msg.createdAt - message.createdAt)}ms` },
+				{ name: `${client.user.username}'s Ping:`, value: `${Math.round(client.ws.ping)}ms` },
+				{ name: `${message.author.username}'s Ping:`, value: `${Math.floor((msg.createdAt - message.createdAt) - client.ws.ping)}ms` },
+			)
+
+		msg.edit(pingembed)
 	},
 };
