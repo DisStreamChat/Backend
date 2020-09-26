bd3edf544b789cf407207b37eca7b9f73e7d2d8c
David Snyder <davidgraygs4@gmail.com>
2020-09-26 17:01:11 -0400

    improve ping command

--- src/Discord/Commands/ping.js
+++ src/Discord/Commands/ping.js
@@ -15,7 +15,7 @@ module.exports = {
 			.addFields(
 				{ name: `**Ping:**`, value: `${Math.floor(msg.createdAt - message.createdAt)}ms` },
 				{ name: `${client.user.username}'s Ping:`, value: `${Math.round(client.ws.ping)}ms` },
-				{ name: `${message.author.username}'s Ping:`, value: `${Math.floor((msg.createdAt - message.createdAt) - client.ws.ping)}ms` },
+				{ name: `${message.author.username}'s Ping:`, value: `${Math.abs(Math.floor((msg.createdAt - message.createdAt) - client.ws.ping))}ms` },
 			)
 
 		msg.edit(pingembed)
