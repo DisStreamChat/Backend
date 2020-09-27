afe0872c9c00564fe10629e5a99656422d9d386f
David Snyder <davidgraygs4@gmail.com>
2020-09-27 07:50:35 -0400

    added events for invite creation and deletion

--- src/Discord/Events/inviteCreate.js
+++ src/Discord/Events/inviteCreate.js
@@ -1,17 +1,27 @@
 import admin from "firebase-admin";
 import { MessageEmbed } from "discord.js";
 
-module.exports = async (invite) => {
-    // const guild = channel.guild;
+module.exports = async invite => {
+    const guild = invite.guild;
+    if(!guild) return
 
-    // let channelId = null;
-    // const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
-    // const serverData = serverRef.data();
-    // if (serverData) {
-    //     channelId = serverData.server;
-    // }
+	let channelId = null;
+	const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
+	const serverData = serverRef.data();
+	if (serverData) {
+		channelId = serverData.server;
+	}
 
-    // if (!channelId) return;
+	const embed = new MessageEmbed()
+		.setAuthor("DisStreamBot")
+		.setDescription(`:inbox_tray: The invite: ${invite.url} **was created**`)
+		.setFooter(`Code: ${invite.code}`)
+		.setTimestamp(new Date())
+		.setColor("#11ee11");
 
-    console.log(invite)
+	if (!channelId) return;
+
+	const logChannel = guild.channels.resolve(channelId);
+
+	logChannel.send(embed);
 };
