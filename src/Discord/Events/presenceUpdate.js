1668b79122e3cf4466e54952a97334fed3586ecc
David Snyder <davidgraygs4@gmail.com>
2020-09-26 21:40:32 -0400

    comment out stuff for presenceUpdate

--- src/Discord/Events/presenceUpdate.js
+++ src/Discord/Events/presenceUpdate.js
@@ -2,16 +2,16 @@ import admin from "firebase-admin";
 import { MessageEmbed } from "discord.js";
 
 module.exports = async (oldPresence, newPresence) => {
-    const guild = channel.guild;
+    // const guild = channel.guild;
 
-    let channelId = null;
-    const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
-    const serverData = serverRef.data();
-    if (serverData) {
-        channelId = serverData.server;
-    }
+    // let channelId = null;
+    // const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
+    // const serverData = serverRef.data();
+    // if (serverData) {
+    //     channelId = serverData.server;
+    // }
 
-    // if (!channelId) return;
+    // // if (!channelId) return;
 
     console.log(oldPresence, newPresence)
 };
