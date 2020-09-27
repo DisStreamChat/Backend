808965f7813b02b39e83fb6d7d532756f58b9ceb
David Snyder <davidgraygs4@gmail.com>
2020-09-26 21:30:56 -0400

    added boilerplate for 10 more event files

---
+++ src/Discord/Events/presenceUpdate.js
@@ -0,0 +1,17 @@
+import admin from "firebase-admin";
+import { MessageEmbed } from "discord.js";
+
+module.exports = async (oldPresence, newPresence) => {
+    const guild = channel.guild;
+
+    let channelId = null;
+    const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
+    const serverData = serverRef.data();
+    if (serverData) {
+        channelId = serverData.server;
+    }
+
+    // if (!channelId) return;
+
+    console.log(oldPresence, newPresence)
+};
