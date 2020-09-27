92622d645e1bd892c54e43882c6d22f550b923c6
David Snyder <davidgraygs4@gmail.com>
2020-09-27 08:16:09 -0400

    added logging for role creation and deletion

--- src/Discord/Events/roleCreate.js
+++ src/Discord/Events/roleCreate.js
@@ -1,17 +1,26 @@
 import admin from "firebase-admin";
 import { MessageEmbed } from "discord.js";
 
-module.exports = async (role) => {
-    // const guild = channel.guild;
+module.exports = async role => {
+    const guild = role.guild;
+    if(!guild) return
 
-    // let channelId = null;
-    // const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
-    // const serverData = serverRef.data();
-    // if (serverData) {
-    //     channelId = serverData.server;
-    // }
+	let channelId = null;
+	const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
+	const serverData = serverRef.data();
+	if (serverData) {
+		channelId = serverData.server;
+    }
+    
+	const embed = new MessageEmbed()
+		.setDescription(`:inbox_tray: The role: ${role} **was created**`)
+		.setFooter(`ID: ${role.id}`)
+		.setTimestamp(role.createdAt)
+		.setColor("#11ee11");
 
-    // if (!channelId) return;
+	if (!channelId) return;
 
-    console.log(role)
+	const logChannel = guild.channels.resolve(channelId);
+
+	logChannel.send(embed);
 };
