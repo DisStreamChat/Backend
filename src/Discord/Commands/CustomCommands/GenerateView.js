021d798e72682212eae9397b54a4f7fff8a00f72
David Snyder <davidgraygs4@gmail.com>
2020-09-29 22:49:39 -0400

    added time to custom command variables

--- src/Discord/Commands/CustomCommands/GenerateView.js
+++ src/Discord/Commands/CustomCommands/GenerateView.js
@@ -1,7 +1,8 @@
 module.exports = ({message, args}) => {
     const view = {
         member: message.member,
-        author: message.author
+        author: message.author,
+        time: () => new Date().toLocaleTimeString(),
     };
 	for (let i = 0; i < args.length; i++) {
 		view[`arg${i + 1}`] = args[i];
