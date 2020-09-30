cf1afacd60fcaf9aad7023e967ad338eeb42122c
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-09-29 22:51:37 -0400

    Revert "Better custom commands (#48)"
    This reverts commit b688ab794baec773bfcf93c31586533490b47e11.

--- src/Discord/CommandHandler.js
+++ src/Discord/CommandHandler.js
@@ -1,17 +1,15 @@
-require("dotenv").config();
 const path = require("path");
 const fs = require("fs");
 const { adminWare, modWare } = require("../utils/functions");
 const commandPath = path.join(__dirname, "Commands");
 const commandFiles = fs.readdirSync(commandPath);
 const commands = {};
-const customCommandHandler = require("./Commands/CustomCommands");
 
 // TODO: use WalkSync to allow for nested folders in command directory
 commandFiles.forEach(command => {
 	if (command.endsWith(".js")) {
 		const commandObj = require(path.join(commandPath, command));
-		const _ = [commandObj.name, ...(commandObj.aliases || [])].map(name => {
+		const _ = [commandObj.name, ...(commandObj.aliases||[])].map(name => {
 			commands[name] = commandObj;
 		});
 	}
@@ -23,22 +21,33 @@ const admin = require("firebase-admin");
 // const prefix = "!";
 
 module.exports = async (message, client) => {
-	if (!client.commands) {
+	if(!client.commands){
 		client.commands = commands;
 	}
-	let prefix = "!";
-	try {
-		const settings = (await admin.firestore().collection("DiscordSettings").doc(message.guild.id).get())?.data();
-		prefix = settings?.prefix || "!";
-	} catch (err) {}
-	if (process.env.BOT_DEV == "true") prefix = "?";
+    let prefix = "!"
+    try{
+        const settings = (await admin.firestore().collection("DiscordSettings").doc(message.guild.id).get())?.data()
+        prefix = settings?.prefix || "!"
+    }catch(err){
+
+	}
 	client.prefix = prefix;
 	if (!message.content.startsWith(prefix)) return;
 	const args = message.content.split(" ");
 	const command = args.shift().slice(1);
 	const commandObj = commands[command];
 	if (!commandObj) {
-		customCommandHandler({ message, args, client, command });
+		const guildRef = await admin.firestore().collection("customCommands").doc(message.guild.id).get();
+		const guildData = guildRef.data();
+		if (guildData) {
+			for(const [key, value] of Object.entries(guildData)){
+                if(key === command || command === value.name || value.aliases.includes(command)){
+                    return await message.channel.send(value.message)
+                }
+            }
+		} else {
+			return;
+		}
 	} else {
 		if (commandObj.adminOnly) await adminWare(message, args, client, commandObj.execute);
 		else if (commandObj.permissions) await modWare(message, args, client, commandObj.permissions, commandObj.execute);
