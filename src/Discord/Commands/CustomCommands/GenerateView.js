cd50e65d26bc28824c42594efceb883f5662f1c2
David Snyder <davidgraygs4@gmail.com>
2020-10-01 17:36:40 -0400

    working on mustache

--- src/Discord/Commands/CustomCommands/GenerateView.js
+++ src/Discord/Commands/CustomCommands/GenerateView.js
@@ -1,11 +1,17 @@
-module.exports = ({message, args}) => {
-    const view = {
-        member: message.member,
-        author: message.author,
-        time: () => new Date().toLocaleTimeString(),
-    };
-	for (let i = 0; i < args.length; i++) {
-		view[`arg${i + 1}`] = args[i];
-    }
-    return view
-}
+module.exports = ({ message, args }) => {
+	const view = {
+		member: message.member,
+		author: message.author,
+		time: () => (val, render) => {
+			try {
+				return new Date().toLocaleTimeString([], { timeZone: val });
+			} catch (err) {
+				return "Invalid TimeZone";
+			}
+		},
+	};
+	// for (let i = 0; i < args.length; i++) {
+	// 	view[`arg${i + 1}`] = args[i];
+	// }
+	return view;
+};
--- src/Discord/Commands/CustomCommands/index.js
+++ src/Discord/Commands/CustomCommands/index.js
@@ -5,14 +5,32 @@ const fs = require("fs");
 import Mustache from "mustache";
 import GenerateView from "./GenerateView";
 
+Mustache.tags = ["{", "}"];
+// Mustache.escape = text => text
+
+const funcRegex = /\((\w+)\s?([\w\s+-/<>]*)\)/gi;
+
+const objectifyArray = array =>
+	array.reduce((acc, cur, i) => {
+		acc[i + 1] = cur;
+		return acc;
+	}, {});
+
+const replaceFunc = text => text.replace(funcRegex, (match, p1, p2, offset, string) => `{#${p1}}${p2 || ""}{/${p1}}`);
+
+const replaceArgs = (text, args) => text.replace(/{(\d+)}/gm, (match, p1, p2, offset, string) => "" + args[+p1 - 1]);
+
 module.exports = async ({ command, args, message, client }) => {
-	const view = GenerateView({message, args})
+	const view = GenerateView({ message, args });
 	const guildRef = await admin.firestore().collection("customCommands").doc(message.guild.id).get();
 	const guildData = guildRef.data();
 	if (guildData) {
 		for (const [key, value] of Object.entries(guildData)) {
-			if (key === command || command === value.name || value.aliases.includes(command)) {
-				return await message.channel.send(Mustache.render(value.message, view, {}, ["{", "}"]).replace(/&lt;/gim, "<").replace(/&gt;/gim, ">"));
+			if (key === command || command === value.name || value?.aliases?.includes?.(command)) {
+				let text = replaceArgs(value.message, args);
+				text = replaceFunc(text);
+				console.log(text);
+				return await message.channel.send(Mustache.render(text, view).replace(/&lt;/gim, "<").replace(/&gt;/gim, ">"));
 			}
 		}
 	} else {
