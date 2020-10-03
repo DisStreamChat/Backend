4409603a9cc46d269807edae16005df011493cc5
David Snyder <davidgraygs4@gmail.com>
2020-10-02 22:08:32 -0400

    added base for stats command

---
+++ src/Discord/Commands/stats.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+import admin from "firebase-admin";
+
+module.exports = {
+	name: "stats",
+	aliases: [],
+    description: "Get DisStreamChat Stats",
+	adminOnly: true,
+	execute: async (message, args, client) => {
+		// const users = /
+	},
+};
