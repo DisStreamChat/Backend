725c2c67aa7d8d20a7b5902a5a590fd62638ce38
David Snyder <davidgraygs4@gmail.com>
2020-10-03 08:46:35 -0400

    added snapshot for default logging settings

--- src/Discord/Events/utils/setupLogging.js
+++ src/Discord/Events/utils/setupLogging.js
@@ -3,26 +3,35 @@ import admin from "firebase-admin";
 let defaultLogging;
 setTimeout(() => {
 	(async () => {
-		defaultLogging = (await admin.firestore().collection("defaults").doc("loggingEvents").get()).data();
-		console.log(defaultLogging);
+		admin
+			.firestore()
+			.collection("defaults")
+			.doc("loggingEvents")
+			.onSnapshot(snapshot => {
+				const data = snapshot.data();
+				if (data) {
+				}
+				defaultLogging = data;
+				console.log(defaultLogging);
+			});
 	})();
 }, 1000);
 
 module.exports = async (guild, id) => {
-    let channelId = null;
-    let active = true
+	let channelId = null;
+	let active = true;
 	const serverRef = await admin.firestore().collection("loggingChannel").doc(guild.id).get();
 	const serverData = serverRef.data();
 	if (serverData) {
 		channelId = serverData.server;
-        const channelOverrides = serverData.channnelOverrides || {};
-        const eventDetails = defaultLogging[id]
-        const category = eventDetails?.category
-        console.log(category)
+		const channelOverrides = serverData.channnelOverrides || {};
+		const eventDetails = defaultLogging[id];
+		const category = eventDetails?.category;
+		console.log(category);
 		const override = channelOverrides[category];
 		if (override) channelId = override;
 		const activeLogging = serverData.activeEvents || {};
 		if (!activeLogging[id]) active = false;
-    }
-    return [channelId, active]
-}
+	}
+	return [channelId, active];
+};
