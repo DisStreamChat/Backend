7fd288b18b9f6922b007c25f886c57fb02a9412c
David Snyder <davidgraygs4@gmail.com>
2020-10-02 21:48:07 -0400

    add ban and unban commands

---
+++ src/Discord/Commands/unban.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const { resolveUser, formatFromNow } = require("../../utils/functions");
+
+module.exports = {
+	name: "unban",
+	aliases: [],
+	description: "Unban a user",
+	usage: ["<user>"],
+	permissions: ["MANAGE_SERVER", "BAN_MEMBERS", "ADMINISTRATOR"],
+	//TODO: check MANAGE_MESSAGES for the channel not the server
+	execute: async (message, args, client) => {
+		if (args.length === 0) {
+			return await message.channel.send(":x: Missing User");
+		}
+		try {
+			let user = await message.guild.fetchBan(args[0].replace(/[\\<>@#&!]/g, ""));
+			if (!user) {
+				return await message.channel.send(":x: User was not banned or doesn't exist");
+			}
+		} catch (err) {
+			return await message.channel.send(":x: User was not banned or doesn't exist");
+		}
+		const nickname = user.username;
+		await message.guild.members.unban(user.id);
+		const embed = new MessageEmbed()
+			.setTitle("Unbanned user")
+			.setAuthor(client.user.tag, client.user.avatarURL())
+			.setDescription(`Unbanned **${nickname}**`);
+		message.channel.send(embed);
+	},
+};
