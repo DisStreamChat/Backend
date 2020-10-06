39c0b354ae31e5a9b17c698832e1257a676fe63e
David Snyder <davidgraygs4@gmail.com>
2020-10-06 09:42:32 -0400

    get variables for reaction events

--- src/Discord/Reaction Roles/ReactionRemove.js
+++ src/Discord/Reaction Roles/ReactionRemove.js
@@ -1,3 +1,13 @@
-module.exports = (reaction, user) => {
+const admin = require("firebase-admin");
 
+module.exports = (reaction, user) => {
+    const message = reaction.message
+    const guild = message.guild
+    const member = guild.members.resolve(user.id)
+    const guildRef = admin.firestore().collection("reactionRoles").doc(guild.id)
+    const guild = await guildRef.get()
+    const guildData = guild.data()
+    const reactionRoleMessage = guildData[message.id] 
+    if(!reactionRoleMessage) return 
+    // handle reaction and assign the correct role
 }
