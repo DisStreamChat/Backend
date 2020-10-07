d0ec6f2044d187e19a676f3dc4d07be3836d84a8
David Snyder <davidgraygs4@gmail.com>
2020-10-07 08:37:40 -0400

    added basic changed detection to roles update

--- src/Discord/Events/roleUpdate.js
+++ src/Discord/Events/roleUpdate.js
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
 import { MessageEmbed } from "discord.js";
-const rdiff = require("recursive-diff");
+// const rdiff = require("recursive-diff");
 
 module.exports = async (oldRole, newRole) => {
-    console.log("role updated")
+	await new Promise(res => setTimeout(res, 300))
 	const guild = oldRole.guild;
 
 	const auditLog = await guild.fetchAuditLogs();
@@ -22,14 +22,28 @@ module.exports = async (oldRole, newRole) => {
 	}
 
 	if (!channelId) return;
-	// let changed = "";
-	// if (oldRold.name !== newRole.name) {
-	// 	changed = "name";
-	// } else if (oldRole.color !== newRole.color) {
-	// 	changed = "color";
-	// } else if(oldRole.hoist !== newRole.hoist){
-
-	// }
-
+	let changed = [];
+	if (oldRole.name !== newRole.name) {
+		changed.push("name");
+	}
+	if (oldRole.color !== newRole.color) {
+		changed.push("color");
+	}
+	if (oldRole.hoist !== newRole.hoist) {
+		changed.push("hoist");
+	}
+	if (oldRole.mentionable !== newRole.mentionable) {
+		changed.push("mentionable");
+	}
+	if(!oldRole.permissions.equals(newRole.permissions.bitfield)){
+		changed.push("permissions")
+	}
+	if(oldRole.rawPosition !== newRole.rawPosition){
+		changed.push("position")
+	}
+	console.log(changed)
+	const changeEmbed = new MessageEmbed()
+	.setTitle("Role Updated")
+	.setAuthor(executor.tag, executor.displayAvatarURL())
 	// console.log(oldRole, newRole)
 };
