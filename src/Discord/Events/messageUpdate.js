92fe2e0016b05510f20823641e8c686d5d32df9e
David Snyder <davidgraygs4@gmail.com>
2020-10-07 10:08:15 -0400

    added bases for final logging events

---
+++ src/Discord/Events/messageUpdate.js
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+import { MessageEmbed } from "discord.js";
+import setupLogging from "./utils/setupLogging";
+
+module.exports = async (oldEmoji, newEmoji) => {
+    // const guild = newEmoji.guild;
+
+    // const [channelId, active] = setupLogging(guild, "emojiUpdate")
+    // if(!active) return
+
+    // if (!channelId) return;
+
+    // // console.log(oldEmoji, newEmoji)
+};
