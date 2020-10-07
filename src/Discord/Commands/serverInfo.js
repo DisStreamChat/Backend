0ba9d9270f38fc3d4c72c43782486edca1a5bbeb
David Snyder <davidgraygs4@gmail.com>
2020-10-07 19:54:40 -0400

    fix permissions on server-info command

--- src/Discord/Commands/serverInfo.js
+++ src/Discord/Commands/serverInfo.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "serverInfo",
 	aliases: ["server-info"],
     description: "Get information about the server",
-    permissions: ["MANAGE_SERVER"],
+    permissions: ["MANAGE_SERVER", "ADMINISTRATOR"],
 	execute: async (message, args, client) => {
         console.log("hello")
         let availableCommands = getCommands(message, client)
