0da99a3545f20f06c60848c093b6174f345c3d73
David Snyder <davidgraygs4@gmail.com>
2020-10-07 23:53:36 -0400

    added basic welcome message functionality

---
+++ src/Discord/Events/misc/WelcomeMessage.js
@@ -0,0 +1,23 @@
+import admin from "firebase-admin";
+const { formatFromNow } = require("../../utils/functions");
+import { MessageEmbed } from "discord.js";
+import generateView from "../../Commands/CustomCommands/GenerateView";
+
+Mustache.tags = ["{", "}"];
+
+module.exports = async (guild, member) => {
+	const settings = await admin.firestore().collection("DiscordSettings").doc(guild.id).get();
+	const settingsData = settings.data();
+	if (!data) return;
+	if (!data.activeEvents["welcome-message"]) return;
+	if (!data.welcomeMessage) return;
+    const view = generateView({ message: { member: member, author: member }, args: [] });
+    const message = Mustache.render(data.welcomeMessage.message, view).replace(/&lt;/gim, "<").replace(/&gt;/gim, ">") 
+    const channelId = data.welcomeMessage.channel
+    if(!channelId) return
+    const welcomeChannel = guild.channels.resolve(channelId);
+
+    // TODO: add welcome card functionality
+
+	welcomeChannel.send(message);
+};
