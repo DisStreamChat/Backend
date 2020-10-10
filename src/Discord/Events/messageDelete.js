821bd53dc8f979d7f5c24c410ed9b9fefcf2da7d
David Snyder <davidgraygs4@gmail.com>
2020-10-09 23:57:47 -0400

    update message delete to check if delete was in the audit log

--- src/Discord/Events/messageDelete.js
+++ src/Discord/Events/messageDelete.js
@@ -3,18 +3,26 @@ import { MessageEmbed } from "discord.js";
 import setupLogging from "./utils/setupLogging";
 import {DiscordClient} from "../../utils/initClients"
 module.exports = async message => {
+    await new Promise(res => setTimeout(res, 500))
 	const guild = message.guild;
 
+    // get the deleter from the guilds audit log
 	const auditLog = await guild.fetchAuditLogs();
 
 	const deleteAction = await auditLog.entries.first();
 
-	let executor = deleteAction.executor;
+    let executor = deleteAction.executor;
+    
+    if(deleteAction.action !== "MESSAGE_DELETE"){
+        executor = null
+    }
 
 	if(DiscordClient.deleter){
 		console.log("bot deleter")
 		executor = DiscordClient.deleter
 	}
+
+    console.log(deleteAction.action, DiscordClient.deleter)
 
 	const { channel, content, author, id } = message;
 
@@ -29,15 +37,15 @@ module.exports = async message => {
 	}
 
 	const embed = new MessageEmbed()
-		.setAuthor(executor.tag, executor.avatarURL())
+		.setAuthor(executor?.tag || "Unknown Deleter", executor?.avatarURL?.())
 		.setTitle("Message Deleted")
-		.setThumbnail(executor.avatarURL())
+		.setThumbnail(executor?.avatarURL())
 		// .setDescription(
 		// 	`:x: A message from ${author || "An unknown user"} was deleted from ${channel} by ${executor}`
         // )
         .addField("Message Sender", `${author || "An unknown user"}`, true)
         .addField("Channel", `${channel}`, true)
-        .addField("Deleted By", `${executor}`, true)
+        .addField("Deleted By", `${executor || "Unknown"}`, true)
         .addField("Message Content", content || "unkown content")
 		.setFooter(`ID: ${id}`)
 		.setTimestamp(new Date())
