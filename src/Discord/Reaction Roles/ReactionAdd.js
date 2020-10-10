8c54b28ff969f6df3b18f878a916bc4f843db74d
David Snyder <davidgraygs4@gmail.com>
2020-10-10 16:56:11 -0400

    renamed roles folder

--- src/Discord/Reaction Roles/ReactionAdd.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-import setup from "./setup";
-module.exports = async (reaction, user) => {
-	const {roleToGive} = await setup(reaction, user);
-    if (!roleToGive) return;
-    const member = await reaction.message.guild.members.resolve(user)
-	await member.roles.add(roleToGive);
-	// handle reaction and assign the correct role
-};
