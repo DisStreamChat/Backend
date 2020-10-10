31d8f4bacf6fd56d5641533ad173d7cce34c630e
David Snyder <davidgraygs4@gmail.com>
2020-10-10 16:58:28 -0400

    minor changes to role manager

--- src/Discord/Reaction Manager/index.js
+++ src/Discord/Reaction Manager/index.js
@@ -1,7 +1,9 @@
 import ReactionAdd from "./ReactionAdd"
 import ReactionRemove from "./ReactionRemove"
+import MemberAdd from "./GuildMemberAdd"
 
 module.exports = (client) => {
     client.on("messageReactionAdd", ReactionAdd)
     client.on("messageReactionRemove", ReactionRemove)
+    client.on("GuildMemberAdd", MemberAdd)
 }
--- src/Discord/Reaction Manager/setup.js
+++ src/Discord/Reaction Manager/setup.js
@@ -4,7 +4,7 @@ module.exports = async (reaction, user, onJoin) => {
 	const message = reaction.message;
 	const guild = message.guild;
 	const member = guild.members.resolve(user);
-	const guildRef = admin.firestore().collection("reactionRoles").doc(guild.id);
+	const guildRef = admin.firestore().collection("reactions").doc(guild.id);
 	const guildDB = await guildRef.get();
 	const guildData = guildDB.data();
 	const reactionRoleMessage = guildData[message.id];
