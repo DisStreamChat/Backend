1092283ac610e7707571fb0af7d9a59e147bfa5f
David Snyder <davidgraygs4@gmail.com>
2020-10-11 10:20:14 -0400

    improve member join for reaction manager

--- src/Discord/Reaction Manager/GuildMemberAdd.js
+++ src/Discord/Reaction Manager/GuildMemberAdd.js
@@ -2,7 +2,7 @@ import setup from "./setup";
 import { addRole, removeRole } from "./misc";
 
 module.exports = async member => {
-	const { roleToGive, DMuser } = await setup(null, user, true);
+	const { roleToGive, DMuser } = await setup({ message: { guild: member.guild, id: "member-join" } }, user, true);
 	if (!roleToGive) return;
-	await addRole({member, role: roleToGive, DMuser})
+	await addRole({ member, role: roleToGive, DMuser });
 };
