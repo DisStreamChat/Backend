e0a6853214c17e3be113e989981073d493f18151
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-15 22:55:00 -0400

    Revert "Leveling settings (#59)"
    This reverts commit 28b81c6e165f2283d5ae14478e939de9e8e18803.

--- src/Discord/Leveling.js
+++ src/Discord/Leveling.js
@@ -1,43 +1,18 @@
 // the admin app has already been initialized in routes/index.js
 const admin = require("firebase-admin");
-const { Random, ArrayAny } = require("../utils/functions");
+const { Random } = require("../utils/functions");
 
 const { getXp } = require("../utils/functions");
-
-const getRoleScaling = (roles, scaling) => {
-	const sortedRoles = roles.sort((a, b) => -1 * a.comparePositionTo(b));
-	for (const role of roles) {
-		const scale = scaling?.[role.id];
-		if (scale != undefined) return scale;
-	}
-};
 
 module.exports = {
 	handleLeveling: async message => {
 		const settings = (await admin.firestore().collection("DiscordSettings").doc(message.guild.id).get()).data();
 		if (!settings?.activePlugins?.leveling) return;
-		const levelingRef = admin.firestore().collection("Leveling").doc(message.guild.id);
-		const levelingDataRef = await levelingRef.get();
-		const levelingSettingsRef = await levelingRef.collection("settings").get();
-		const levelingSettings = levelingSettingsRef.docs.reduce((acc, cur) => ({ ...acc, [cur.id]: cur.data() }), {});
+		const levelingDataRef = await admin.firestore().collection("Leveling").doc(message.guild.id).get();
 		const levelingData = levelingDataRef.data();
 		if (levelingData) {
-			const channel = message.channel;
-			const channelsToIgnore = levelingSettings?.bannedItems?.channels || [];
-			if (channelsToIgnore.includes(channel.id)) return;
-			const rolesToIgnore = levelingSettings?.bannedItems?.roles || [];
-			const member = message.member;
-			if (
-				ArrayAny(
-					rolesToIgnore,
-					member.roles.cache.array().map(role => role.id)
-				)
-			)
-				return;
-			const generalScaling = levelingSettings?.scaling?.general;
-			const roleScaling = getRoleScaling(member.roles.cache.array(), levelingSettings?.scaling?.roles || {});
-			const finalScaling = roleScaling ?? generalScaling ?? 1;
 			const levelingChannelId = levelingData.type === 3 ? levelingData.notifications || message.channel.id : message.channel.id;
+			// console.log(levelingChannelId);
 			let userLevelingData = levelingData[message.author.id];
 			if (!userLevelingData) {
 				userLevelingData = { xp: 0, level: 0, cooldown: 0 };
@@ -47,7 +22,7 @@ module.exports = {
 			const expireTime = userLevelingData.cooldown + cooldownTime;
 			if (now > expireTime) {
 				userLevelingData.cooldown = now;
-				userLevelingData.xp += Random(10, 20) * finalScaling;
+				userLevelingData.xp += Random(10, 20);
 				userLevelingData.xp = Math.floor(userLevelingData.xp);
 				let xpToNextLevel = getXp(userLevelingData.level + 1);
 				if (userLevelingData.xp >= xpToNextLevel) {
