8467ea889f0966be973a4b6b62228635d5c63d07
David Snyder <davidgraygs4@gmail.com>
2020-10-19 13:19:44 -0400

    fix typo

--- src/Discord/Events/misc/WelcomeMessage.js
+++ src/Discord/Events/misc/WelcomeMessage.js
@@ -10,7 +10,7 @@ module.exports = async (guild, member) => {
 	const settings = await admin.firestore().collection("DiscordSettings").doc(guild.id).get();
 	const data = settings.data();
 	if (!data) return;
-	if (!data.activeEvents["welcome-message"]) return;
+	if (!data.activePlugins["welcome-message"]) return;
 	if (!data.welcomeMessage) return;
     const view = generateView({ message: { member: member, author: member }, args: [] });
     const message = Mustache.render(data.welcomeMessage.message, view).replace(/&lt;/gim, "<").replace(/&gt;/gim, ">") 
