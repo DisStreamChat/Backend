4112571ab737635e358a5690ffb693eb8934838d
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-18 21:32:21 -0400

    Custom bots (#62)
    * minor changes
    
    * changed functions to work with custom bots and add custom bots

--- src/Discord/Reaction Manager/index.js
+++ src/Discord/Reaction Manager/index.js
@@ -1,9 +1,9 @@
-import ReactionAdd from "./ReactionAdd"
-import ReactionRemove from "./ReactionRemove"
-import MemberAdd from "./GuildMemberAdd"
+import ReactionAdd from "./ReactionAdd";
+import ReactionRemove from "./ReactionRemove";
+import MemberAdd from "./GuildMemberAdd";
 
-module.exports = (client) => {
-    client.on("messageReactionAdd", ReactionAdd)
-    client.on("messageReactionRemove", ReactionRemove)
-    client.on("guildMemberAdd", MemberAdd)
-}
+module.exports = client => {
+	client.on("messageReactionAdd", (...params) => ReactionAdd(...params, client));
+	client.on("messageReactionRemove", (...params) => ReactionRemove(...params, client));
+	client.on("guildMemberAdd", (...params) => MemberAdd(...params, client));
+};
