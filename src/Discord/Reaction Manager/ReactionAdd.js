036ff38dd3fabc72ddc74cdf6a5ee2ee70fc9f11
David Snyder <davidgraygs4@gmail.com>
2020-10-28 08:43:10 -0400

    add toggle reverse type

--- src/Discord/Reaction Manager/ReactionAdd.js
+++ src/Discord/Reaction Manager/ReactionAdd.js
@@ -2,13 +2,18 @@ import setup from "./setup";
 import { addRole, removeRole } from "./misc";
 
 module.exports = async (reaction, user, DiscordClient) => {
-	console.log("role added")
+	console.log("role added");
 	let { member, type, role: roles, DMuser, rolesToGive } = await setup(reaction, user);
 	if (!Array.isArray(roles)) roles = [roles];
 	for (const roleToGive of rolesToGive) {
-		const role = roleToGive.id
+		const role = roleToGive.id;
 		switch (type) {
 			case "REMOVE_ON_ADD":
+				if (member.roles.cache.has(role)) {
+					await removeRole({ role: roleToGive, member, DMuser, DiscordClient });
+				}
+				break;
+			case "TOGGLE_REVERSED":
 				if (member.roles.cache.has(role)) {
 					await removeRole({ role: roleToGive, member, DMuser, DiscordClient });
 				}
--- src/Discord/Reaction Manager/ReactionRemove.js
+++ src/Discord/Reaction Manager/ReactionRemove.js
@@ -6,9 +6,14 @@ module.exports = async (reaction, user, DiscordClient) => {
 	let { type, DMuser, role: roles, rolesToGive, member } = await setup(reaction, user);
 	if (!Array.isArray(roles)) roles = [roles];
 	for (const roleToGive of rolesToGive) {
-		const role = roleToGive.id
+		const role = roleToGive.id;
 		switch (type) {
 			case "REMOVE_ON_REMOVE":
+			case "TOGGLE_REVERSED":
+				if (!member.roles.cache.has(role)) {
+					await addRole({ role: roleToGive, member, DMuser, DiscordClient });
+				}
+				break;
 			case "TOGGLE":
 				if (member.roles.cache.has(role)) {
 					await removeRole({ role: roleToGive, member, DMuser, DiscordClient });
