ae86d80549ff8a2f5e52a96bdf9d3123bd73fb61
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-28 08:44:07 -0400

    Role 2 0 (#64)
    * move member fetching to setup
    
    * made roles work with multiple roles
    
    * made member join work with an array of roles
    
    * add toggle reverse type

--- src/Discord/Reaction Manager/ReactionRemove.js
+++ src/Discord/Reaction Manager/ReactionRemove.js
@@ -1,30 +1,29 @@
 import setup from "./setup";
 import { addRole, removeRole } from "./misc";
-import { resolveUser } from "../../utils/functions";
 
 module.exports = async (reaction, user, DiscordClient) => {
 	console.log("role removed");
-	const { roleToGive, type, DMuser, role } = await setup(reaction, user);
-	// console.log({ roleToGive, type, DMuser, role, user });
-	if (!roleToGive) return;
-	let member = await reaction.message.guild.members.resolve(user);
-	if (!member) {
-		member = reaction.message.guild.members.cache.get(user.id);
-	}
-	if (!member) {
-		member = resolveUser(reaction.message, user.id || user.username);
-	}
-	switch (type) {
-		case "REMOVE_ON_REMOVE":
-		case "TOGGLE":
-			if (member.roles.cache.has(role)) {
-				await removeRole({ role: roleToGive, member, DMuser, DiscordClient });
-			}
-			break;
-		case "ADD_ON_REMOVE":
-			if (!member.roles.cache.has(role)) {
-				await addRole({ role: roleToGive, member, DMuser, DiscordClient });
-			}
-			break;
+	let { type, DMuser, role: roles, rolesToGive, member } = await setup(reaction, user);
+	if (!Array.isArray(roles)) roles = [roles];
+	for (const roleToGive of rolesToGive) {
+		const role = roleToGive.id;
+		switch (type) {
+			case "REMOVE_ON_REMOVE":
+			case "TOGGLE_REVERSED":
+				if (!member.roles.cache.has(role)) {
+					await addRole({ role: roleToGive, member, DMuser, DiscordClient });
+				}
+				break;
+			case "TOGGLE":
+				if (member.roles.cache.has(role)) {
+					await removeRole({ role: roleToGive, member, DMuser, DiscordClient });
+				}
+				break;
+			case "ADD_ON_REMOVE":
+				if (!member.roles.cache.has(role)) {
+					await addRole({ role: roleToGive, member, DMuser, DiscordClient });
+				}
+				break;
+		}
 	}
 };
