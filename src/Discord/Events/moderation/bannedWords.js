df832bfc0ab53b029d8a92dd0ea3b5fc5391c511
David Snyder <davidgraygs4@gmail.com>
2020-11-22 07:46:07 -0500

    change variable name

--- src/Discord/Events/moderation/bannedWords.js
+++ src/Discord/Events/moderation/bannedWords.js
@@ -4,7 +4,8 @@ import { getDiscordSettings } from "../../../utils/functions";
 module.exports = async (message, client) => {
 	const settings = await getDiscordSettings({ client, guild: message.guild.id });
 	const guildRef = admin.firestore().collection("moderation").doc(message.guild.id);
-	const guildObj = await guildRef.get();
-	const guildData = guildRef.data();
+	const guildDoc = await guildRef.get();
+	const guildData = guildDoc.data();
 	if (!guildData) return;
+	
 };
