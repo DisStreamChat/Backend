5d7167e43999e5c266707677f021184ab4514b16
David Snyder <davidgraygs4@gmail.com>
2020-11-22 09:19:13 -0500

    start to add in more moderation events

---
+++ src/Discord/Events/moderation/externalLinks.js
@@ -0,0 +1,22 @@
+import admin from "firebase-admin";
+import { getDiscordSettings, informMods, warn } from "../../../utils/functions";
+
+module.exports = async (message, client) => {
+	const settings = await getDiscordSettings({ client, guild: message.guild.id });
+	const guildRef = admin.firestore().collection("moderation").doc(message.guild.id);
+	const guildDoc = await guildRef.get();
+	const guildData = guildDoc.data();
+	if (!guildData) return;
+	if (guildData.bannedWords) {
+		for (const word of guildData.bannedWords) {
+			if (message.content.includes(word)) {
+				if (guildData.subtle) {
+					informMods(message, guild, client);
+				} else {
+					message.delete();
+					warn(message.member, guild, client);
+				}
+			}
+		}
+	}
+};
