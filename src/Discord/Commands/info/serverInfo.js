4863233acb12b2d422db7482954a4403a35ef631
David Snyder <davidgraygs4@gmail.com>
2020-11-23 22:10:08 -0500

    implement serverinfo command

--- src/Discord/Commands/info/serverInfo.js
+++ src/Discord/Commands/info/serverInfo.js
@@ -1,14 +1,29 @@
+import { MessageEmbed } from "discord.js";
+
 module.exports = {
 	name: "serverInfo",
-	aliases: ["server-info"],
+	aliases: ["server-info", "serverinfo"],
 	id: "server-info",
 	category: "info",
-    description: "Get information about the server",
-    permissions: ["MANAGE_SERVER", "ADMINISTRATOR"],
+	description: "Get information about the server",
 	execute: async (message, args, client) => {
-        console.log("hello")
-        let availableCommands = getCommands(message, client)
-        console.log(availableCommands)
-        message.channel.send(availableCommands.map(command => command.displayName).join("\n"))
+		const guild = message.guild;
+		const owner = guild.owner;
+
+		const embed = new MessageEmbed()
+			.setAuthor(guild.name, guild.iconURL())
+			.setTitle(`Info about ${guild.name}`)
+			.addField("Owner", owner, true)
+			.addField("Partnered", guild.partnered ? "Yes" : "No", true)
+			.addField("Region", guild.region, true)
+			.addField("Categories", [...new Set(guild.channels.cache.array().map(channel => channel.parentID))].length, true)
+			.addField("Text Channels", guild.channels.cache.array().filter(channel => channel.type === "text").length, true)
+			.addField("Voice Channels", guild.channels.cache.array().filter(channel => channel.type === "voice").length, true)
+			.addField("Members", guild.memberCount, true)
+			.addField(`Roles - ${guild.roles.cache.array().length}`, guild.roles.cache.array().join(", "))
+			.setFooter(`ID: ${guild.id} | Server Created`)
+			.setTimestamp(guild.createdAt);
+
+		message.channel.send(embed);
 	},
 };
