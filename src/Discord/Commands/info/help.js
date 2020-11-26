a46b4fde843e76d90a268ba33979a15ff2ce2d3a
David Snyder <davidgraygs4@gmail.com>
2020-11-25 21:06:48 -0500

    added pages to help command embeds

--- src/Discord/Commands/info/help.js
+++ src/Discord/Commands/info/help.js
@@ -1,6 +1,6 @@
 const { isAdmin, hasPermission, ArrayAny, getRoleIds } = require("../../../utils/functions");
 const { MessageEmbed } = require("discord.js");
-import { getDiscordSettings } from "../../../utils/functions";
+import { getDiscordSettings, convertDiscordRoleColor } from "../../../utils/functions";
 
 // the admin app has already been initialized in routes/index.js
 const admin = require("firebase-admin");
@@ -75,6 +75,37 @@ const addTips = async (embed, msg, client) => {
 	return embed;
 };
 
+const maxCommands = 2;
+const generateHelpEmbed = async ({ message, client, commands, custom, page = 1 }) => {
+	const pages = custom ? Math.ceil(commands.length / (maxCommands * 4)) : Math.ceil(Object.keys(commands || {}).length / maxCommands);
+	const index = (page - 1) * maxCommands;
+	const helpEmbed = new MessageEmbed()
+		.setTitle("DisStreambot Help")
+		.setDescription(`Here are all the available commands.`)
+		.addField("Prefix", client.prefix || "!")
+		.setThumbnail(client.user.displayAvatarURL())
+		.setAuthor("DisStreamBot Commands", client.user.displayAvatarURL())
+		.setTimestamp(message.createdAt)
+		.setFooter(`Page ${page}/${pages}`)
+		.setColor(convertDiscordRoleColor(message.guild.me.displayHexColor))
+		.addField(
+			"Available Commands",
+			custom
+				? commands
+						.slice(index, index + maxCommands*4)
+						.map(command => (command.displayName.includes("<:") ? command.displayName : `\`${command.displayName}\``))
+						.join(", ")
+				: Object.entries(commands)
+						.slice(index, index + maxCommands)
+						.map(([key, value]) => {
+							return `**${key}**\n${value.map(command => `\`${command.displayName}\``).join(", ")}\n`;
+						})
+		)
+		.addField("---------------------------------------------------", "--------------------------------------------------");
+
+	return { maxPages: pages, embed: await addTips(helpEmbed) };
+};
+
 module.exports = {
 	name: "help",
 	id: "help",
@@ -96,23 +127,47 @@ module.exports = {
 		}, {});
 		const allCommandCategories = { ...commandCategories, custom: customCommands };
 		if (args.length === 0) {
-			const helpEmbed = await addTips(
-				new MessageEmbed()
-					.setTitle("DisStreambot Help")
-					.setDescription(`Here are all the available commands.`)
-					.addField("Prefix", client.prefix || "!")
-					.setThumbnail(client.user.displayAvatarURL())
-					.setAuthor("DisStreamBot Commands", client.user.displayAvatarURL())
-					.addField(
-						"Available Commands",
-						Object.entries(allCommandCategories).map(([key, value]) => {
-							return `**${key}**\n${value.map(command => `\`${command.displayName}\``).join(", ")}\n`;
-						})
-					)
-					.setTimestamp(message.createdAt)
-					.setColor("#206727")
+			const { embed: helpEmbed, maxPages } = await generateHelpEmbed({ message, client, commands: allCommandCategories, page: 1 });
+			const helpMsg = await message.channel.send(helpEmbed);
+			const pageCollector = helpMsg.createReactionCollector(
+				(reaction, user) => ["⬅️", "➡️", "❌"].includes(reaction.emoji.name) && !user.bot
 			);
-			await message.channel.send(helpEmbed);
+			let currentPage = 1;
+			await helpMsg.react("⬅️");
+			await helpMsg.react("➡️");
+			await helpMsg.react("❌");
+			pageCollector.on("end", () => {
+				helpMsg.reactions.removeAll();
+			});
+			pageCollector.on("collect", async reaction => {
+				let pageChanged = false;
+				switch (reaction.emoji.name) {
+					case "⬅️":
+						if (currentPage > 1) {
+							currentPage -= 1;
+							pageChanged = true;
+						}
+						break;
+					case "➡️":
+						if (currentPage < maxPages) {
+							currentPage += 1;
+							pageChanged = true;
+						}
+						break;
+					case "❌":
+						pageCollector.stop();
+						break;
+					default:
+						return;
+				}
+				const senders = reaction.users.cache.array().filter(user => user.id !== client.user.id);
+				for (const sender of senders) {
+					reaction.users.remove(sender);
+				}
+				if (pageChanged) {
+					helpMsg.edit((await generateHelpEmbed({ message, client, commands: allCommandCategories, page: currentPage })).embed);
+				}
+			});
 		} else if (!["module", "admin", "commands"].includes(args[0])) {
 			const selectedCommand = allCommands.find(command => command.displayName?.toLowerCase() === args[0]?.toLowerCase());
 			const commandHelpEmbed = getHelpText({ message, client, selectedCommand });
@@ -133,25 +188,63 @@ module.exports = {
 						}
 					} else {
 						const availableCustomCommands = await getCommands(message, { commands: guildData });
-						const customHelpEmbed = await addTips(
-							new MessageEmbed()
-								.setTitle("DisStreambot Help")
-								.setDescription("Here are all the available custom commands")
-								.setThumbnail(client.user.displayAvatarURL())
-								.setAuthor("DisStreamBot Commands", client.user.displayAvatarURL())
-								.addField(
-									"Available Commands",
-									availableCustomCommands
-										.map(command =>
-											command.displayName.includes("<:") ? command.displayName : `\`${command.displayName}\``
-										)
-										.join(", ")
-								)
-								.addField("Tip", "Type `help commands <command name>` to get help on a specific command ")
-								.setTimestamp(message.createdAt)
-								.setColor("#206727")
+						const { embed: customHelpEmbed, maxPages } = await generateHelpEmbed({
+							message,
+							client,
+							commands: availableCustomCommands,
+							custom: true,
+							page: 1,
+						});
+						const helpMsg = await message.channel.send(customHelpEmbed);
+						const pageCollector = helpMsg.createReactionCollector(
+							(reaction, user) => ["⬅️", "➡️", "❌"].includes(reaction.emoji.name) && !user.bot
 						);
-						await message.channel.send(customHelpEmbed);
+						let currentPage = 1;
+						await helpMsg.react("⬅️");
+						await helpMsg.react("➡️");
+						await helpMsg.react("❌");
+						pageCollector.on("end", () => {
+							helpMsg.reactions.removeAll();
+						});
+						pageCollector.on("collect", async reaction => {
+							let pageChanged = false;
+							switch (reaction.emoji.name) {
+								case "⬅️":
+									if (currentPage > 1) {
+										currentPage -= 1;
+										pageChanged = true;
+									}
+									break;
+								case "➡️":
+									if (currentPage < maxPages) {
+										currentPage += 1;
+										pageChanged = true;
+									}
+									break;
+								case "❌":
+									pageCollector.stop();
+									break;
+								default:
+									return;
+							}
+							const senders = reaction.users.cache.array().filter(user => user.id !== client.user.id);
+							for (const sender of senders) {
+								reaction.users.remove(sender);
+							}
+							if (pageChanged) {
+								helpMsg.edit(
+									(
+										await generateHelpEmbed({
+											message,
+											client,
+											commands: availableCustomCommands,
+											custom: true,
+											page: currentPage,
+										})
+									).embed
+								);
+							}
+						});
 					}
 					break;
 			}
