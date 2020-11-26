f638ac4f8a221e200de1d3a98cf3f99718bd8b65
David Snyder <davidgraygs4@gmail.com>
2020-11-26 10:59:03 -0500

    improve restart command

--- src/Discord/Commands/admin/restart.js
+++ src/Discord/Commands/admin/restart.js
@@ -8,9 +8,10 @@ module.exports = {
     description: "Restarts the Client.",
 	adminOnly: true,
 	execute: async (message, args, client) => {
+		const token = client.token
 		const msg = await message.channel.send("Restarting...");
 		await client.destroy();
-		await client.login(process.env.BOT_TOKEN);
-		await message.channel.send("Restarted");
+		await client.login(token);
+		await msg.edit("Restarted");
 	},
 };
