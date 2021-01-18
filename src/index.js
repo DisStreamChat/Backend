466fa0dd6dd52264c52749ef8b1902b6789e41d7
David Snyder <davidgraygs4@gmail.com>
2021-01-17 23:32:59 -0500

    move express app to seperate file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 require("dotenv").config();
-import express from "express";
-const app = express();
-import http from "http";
-const server = http.Server(app);
-import socketio from "socket.io";
-const io = socketio(server);
-import cors from "cors";
-import bodyParser from "body-parser";
-import helmet from "helmet";
 import TwitchEvents from "./Twitch/TwitchEvents";
 import DiscordEvents from "./Discord/DiscordEvents";
-import crypto from "crypto";
 import { log } from "./utils/functions/logging";
 import {sockets} from "./Sockets"
 // get the initialized clients from another file
 const { DiscordClient, TwitchClient, customBots } = require("./utils/initClients");
-const { ArrayAny } = require("./utils/functions");
 const admin = require("firebase-admin");
+import {io, server, app} from "./app"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SETUP
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 try {
 	// get the serviceAccount details from the base64 string stored in environment variables
@@ -28,36 +22,6 @@ try {
 	});
 } catch (err) {}
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// SETUP
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// add the basic middleware to the express app
-app.use(helmet());
-app.use(cors());
-
-// this function is used to verify twitch webhook requests
-app.use(
-	bodyParser.json({
-		verify: function (req, res, buf, encoding) {
-			// is there a hub to verify against
-			req.twitch_hub = false;
-			if (req.headers && req.headers["x-hub-signature"]) {
-				req.twitch_hub = true;
-
-				var xHub = req.headers["x-hub-signature"].split("=");
-
-				req.twitch_hex = crypto.createHmac(xHub[0], process.env.WEBHOOK_SECRET).update(buf).digest("hex");
-				req.twitch_signature = xHub[1];
-			}
-		},
-	})
-);
-
-// add the routes stored in the 'routes' folder to the app
-app.use("/", require("./routes/index"));
-app.use("/public", express.static("public"));
-app.use("/images", express.static("images"));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TWITCH
