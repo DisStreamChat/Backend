821c3daaefa206aa1edaa89772ae0f4de6f31514
David Snyder <davidgraygs4@gmail.com>
2021-01-25 17:30:51 -0500

    add emoji updated logger

--- src/Discord/Events/emojiUpdate.js
+++ src/Discord/Events/emojiUpdate.js
@@ -1,6 +1,7 @@
 import admin from "firebase-admin";
 import { MessageEmbed } from "discord.js";
 import setupLogging from "./utils/setupLogging";
+import { logUpdate } from "./utils";
 
 module.exports = async (oldEmoji, newEmoji, client) => {
     const guild = newEmoji.guild;
@@ -8,7 +9,16 @@ module.exports = async (oldEmoji, newEmoji, client) => {
     const [channelId, active] = await setupLogging(guild, "emojiUpdate", client)
     if(!active) return
 
+	const embed = await logUpdate(newEmoji, oldEmoji, {
+		title: `:pencil: Emoji updated: ${newEmoji}`,
+		footer: `Emoji ID: ${oldEmoji.id}`,
+		ignoredDifferences: ["_roles"]
+	})
+
     if (!channelId) return;
 
+	const logChannel = guild.channels.resolve(channelId);
+
+	logChannel.send(embed);
     // console.log(oldEmoji, newEmoji)
 };
