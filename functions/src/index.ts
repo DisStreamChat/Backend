43f9be0bc653bc5719852437adac5f282613a9d9
David Snyder <davidgraygs4@gmail.com>
2021-03-05 01:53:11 -0500

    add discord bot the firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,29 @@
 //@ts-ignore
 import * as functions from "firebase-functions";
-// import * as discord from "discord.js";
+import * as discord from "discord.js";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-// const DiscordClient = new discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const DiscordClient = new discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
-// const runBot = async (callback: (client: discord.Client) => Promise<void>) => {
-// 	await DiscordClient.login(functions.config().discord.token);
-
-// 	DiscordClient.once("ready", async () => {
-// 		await callback(DiscordClient);
-// 		DiscordClient.destroy();
-// 	});
-// };
+const runBot = async (callback: (client: discord.Client) => Promise<void>) => {
+	DiscordClient.once("ready", async () => {
+		await callback(DiscordClient);
+		DiscordClient.destroy();
+	});
+	
+	await DiscordClient.login(functions.config().discord.token);
+};
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //@ts-ignore
 export const helloWorld = functions.https.onRequest(async (_request, response) => {
-	// await runBot(async client => {
-	// 	const guild = await client.guilds.fetch("809234741018361916");
-	// 	const channel = guild.channels.resolve("809234742482305087") as discord.TextChannel;
-	// 	if (!channel) return;
-	// 	await channel.send("hello world");
-	// });
-	console.log(functions.config().discord.token)
-	response.json({ message: "hello world" });
+	await runBot(async client => {
+		const guild = await client.guilds.fetch("809234741018361916");
+		const channel = guild.channels.resolve("809234742482305087") as discord.TextChannel;
+		if (!channel) return;
+		await channel.send("hello world");
+	});
+	response.json({ message: "hello world", token: functions.config().discord.token });
 });
