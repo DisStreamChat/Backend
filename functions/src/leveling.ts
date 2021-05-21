5cde17110f3d1dffe4bab62e839a0b4ef5ee0653
David Snyder <david.snyder@induro.io>
2021-05-21 13:29:40 -0400

    add function to aggregate rank cards

--- functions/src/leveling.ts
+++ functions/src/leveling.ts
@@ -1,6 +1,21 @@
 import {firestore} from "firebase-admin";
-import {} from "firebase-functions";
+import * as functions from "firebase-functions";
 
-// const aggregateRank = 
+const aggregateRank = functions.firestore.document("Leveling/{guildId}/users/{userId}").onUpdate(async (change, context) => {
+	const {guildId, userId} = context.params
 
-export const leveling = {};
+	const guildRef = firestore().collection("Leveling").doc(guildId).collection("users")
+
+	const docRef = guildRef.doc(userId)
+
+	const sorted = (
+		await guildRef.orderBy("xp", "desc").get()
+	).docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+	let rank = sorted.findIndex(entry => entry.id === userId) + 1;
+	if (rank === 0) rank = sorted.length + 1;
+
+	docRef.update({rank})
+})
+
+export const leveling = {aggregateRank};
