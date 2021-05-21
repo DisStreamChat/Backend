fa8b13936c3cd7884ed2a3416708c237631e47c7
David Snyder <davidgraygs4@gmail.com>
2021-05-21 16:45:31 -0400

    remove console.time

--- src/Discord/Commands/leveling/rank.ts
+++ src/Discord/Commands/leveling/rank.ts
@@ -12,7 +12,6 @@ export default {
 	description: "Get someones experience and level on this server in a rankcard.",
 	usage: ["(user)"],
 	execute: async (message, args, client) => {
-		console.time("function");
 		let user = await resolveUser(message, args.join(" "));
 		let msg = "";
 		if (!user) {
@@ -25,7 +24,6 @@ export default {
 			return await message.channel.send(`❌ ${user} is a bot and bots don't level.`);
 		}
 
-		console.time("user fetch");
 		let userData = (await admin.firestore().collection("Leveling").doc(message.guild.id).collection("users").doc(user.id).get()).data();
 		let customRankCardData = userData;
 		if (!customRankCardData.primaryColor) {
@@ -33,14 +31,10 @@ export default {
 				await admin.firestore().collection("Streamers").where("discordId", "==", user.id).get()
 			).docs[0]?.data?.();
 		}
-		console.timeEnd("user fetch");
 
 		if (!userData) userData = { xp: 0, level: 0, rank: 100000 };
-		console.time("start generating");
 		const rankCard = await generateRankCard({ ...userData, ...(customRankCardData || {}) }, user);
 		const attachment = new MessageAttachment(rankCard, "card.png");
-		console.timeEnd("start generating");
-		console.timeEnd("function");
 		message.channel.send(msg, attachment);
 	},
 };
